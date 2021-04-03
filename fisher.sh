022c3a775aa861d175ac1efcf0dcb3fabd5940ec
megn <megn@megn.com>
2021-04-03 10:22:11 -0700

    fixed log folder not being tracked and made repo structure more robust

--- fisher.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd ~/git-fishgaman #replace this path with the current path of the folder of fishgaman on YOUR computer.
-while true; do
-	./fish.py
-	sleep 3600
-done
---
+++ src/fisher.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cd ~/git-fishgaman/src #replace this path with the current path of the folder of fishgaman on YOUR computer.
+while true; do
+	./fish.py
+	sleep 3600
+done
